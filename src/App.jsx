2239d2f0785f99a27039dde1f5c4f98589cb059b
juliusmwass <juliusmwass100@gmail.com>
2025-10-15 10:38:03 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-2xl text-blue-600 p-5'> Welcome to my website</h1>
+      <img src="/public/download (9).jpeg" alt="" />
+    </>
+  )
+}
+
+export default App
